45e1e114223ed4c9f4386003473d5f910c6c3bc6
Eric Biggers <ebiggers@google.com>
2019-05-20 09:29:42 -0700

    fscrypt: clean up some BUG_ON()s in block encryption/decryption
    [ Upstream commit eeacfdc68a104967162dfcba60f53f6f5b62a334 ]
    
    Replace some BUG_ON()s with WARN_ON_ONCE() and returning an error code,
    and move the check for len divisible by FS_CRYPTO_BLOCK_SIZE into
    fscrypt_crypt_block() so that it's done for both encryption and
    decryption, not just encryption.
    
    Reviewed-by: Chandan Rajendra <chandan@linux.ibm.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    # Conflicts:
    #	fs/crypto/crypto.c

--- fs/crypto/crypto.c
+++ fs/crypto/crypto.c
@@ -172,7 +172,10 @@ int fscrypt_do_page_crypto(const struct inode *inode, fscrypt_direction_t rw,
 	sdp_fs_command_t *cmd = NULL;
 #endif
 
-	BUG_ON(len == 0);
+	if (WARN_ON_ONCE(len <= 0))
+		return -EINVAL;
+	if (WARN_ON_ONCE(len % FS_CRYPTO_BLOCK_SIZE != 0))
+		return -EINVAL;
 
 	fscrypt_generate_iv(&iv, lblk_num, ci);
 
@@ -299,8 +302,6 @@ struct page *fscrypt_encrypt_page(const struct inode *inode,
 	}
 #endif
 
-	BUG_ON(len % FS_CRYPTO_BLOCK_SIZE != 0);
-
 	if (inode->i_sb->s_cop->flags & FS_CFLG_OWN_PAGES) {
 		/* with inplace-encryption we just encrypt the page */
 		err = fscrypt_do_page_crypto(inode, FS_ENCRYPT, lblk_num, page,
@@ -312,7 +313,8 @@ struct page *fscrypt_encrypt_page(const struct inode *inode,
 		return ciphertext_page;
 	}
 
-	BUG_ON(!PageLocked(page));
+	if (WARN_ON_ONCE(!PageLocked(page)))
+		return ERR_PTR(-EINVAL);
 
 	ctx = fscrypt_get_ctx(inode, gfp_flags);
 	if (IS_ERR(ctx))
@@ -364,8 +366,9 @@ int fscrypt_decrypt_page(const struct inode *inode, struct page *page,
 	if (__fscrypt_inline_encrypted(inode))
 		crypto_diskcipher_debug(FS_DEC_WARN, 0);
 #endif
-	if (!(inode->i_sb->s_cop->flags & FS_CFLG_OWN_PAGES))
-		BUG_ON(!PageLocked(page));
+	if (WARN_ON_ONCE(!PageLocked(page) &&
+			 !(inode->i_sb->s_cop->flags & FS_CFLG_OWN_PAGES)))
+		return -EINVAL;
 #ifdef CONFIG_DDAR
 	if (fscrypt_dd_encrypted_inode(inode)) {
 		// Invert crypto order. OEM crypto must perform after 3rd party crypto
