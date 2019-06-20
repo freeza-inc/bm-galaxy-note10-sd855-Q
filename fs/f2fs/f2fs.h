b34d3705e4cb6f800fbcaaac79bff67033c2a8c2
Chao Yu <yuchao0@huawei.com>
2019-06-20 11:36:14 +0800

    f2fs: use generic EFSBADCRC/EFSCORRUPTED
    [ Upstream commit 10f966bbf521bb9b2e497bbca496a5141f4071d0 ]
    
    f2fs uses EFAULT as error number to indicate filesystem is corrupted
    all the time, but generic filesystems use EUCLEAN for such condition,
    we need to change to follow others.
    
    This patch adds two new macros as below to wrap more generic error
    code macros, and spread them in code.
    
    EFSBADCRC	EBADMSG		/* Bad CRC detected */
    EFSCORRUPTED	EUCLEAN		/* Filesystem is corrupted */
    
    Reported-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    # Conflicts:
    #	fs/f2fs/checkpoint.c
    #	fs/f2fs/segment.c
    #	fs/f2fs/super.c

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -3971,3 +3971,7 @@ static inline bool is_journalled_quota(struct f2fs_sb_info *sbi)
 }
 
 #endif
+
+#define EFSBADCRC	EBADMSG		/* Bad CRC detected */
+#define EFSCORRUPTED	EUCLEAN		/* Filesystem is corrupted */
+
