6dd58e0918e74746e5160f383201e72ea2f4396f
freeza <freeza@gmail.com>
2019-06-21 10:23:06 -0700

    compile with knox_ncm disabled
    Signed-off-by: freeza <freeza@gmail.com>
    
    # Conflicts:
    #	net/core/sock.c
    #	net/ipv4/udp.c
    #	net/netfilter/nf_conntrack_core.c

--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -58,9 +58,11 @@
 #include <net/netns/hash.h>
 
 #include "nf_internals.h"
+#ifdef CONFIG_KNOX_NCM
 // KNOX NPA - START
 #include <net/ncm.h>
 // KNOX NPA - END
+#endif
 
 #define NF_CONNTRACK_VERSION	"0.5.0"
 
@@ -366,6 +368,7 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 {
 	struct ct_pcpu *pcpu;
 
+#ifdef CONFIG_KNOX_NCM
 	// KNOX NPA - START
 	/* Add 'del_timer(&ct->npa_timeout)' if struct nf_conn->timeout is of type struct timer_list; */
 	/* send dying conntrack entry to collect data */
@@ -373,6 +376,7 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 		knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_CLOSE, 10);
 	}
 	// KNOX NPA - END
+#endif
 
 	/* add this conntrack to the (per cpu) dying list */
 	ct->cpu = smp_processor_id();
@@ -1228,9 +1232,11 @@ __nf_conntrack_alloc(struct net *net,
 		     gfp_t gfp, u32 hash)
 {
 	struct nf_conn *ct;
+#ifdef CONFIG_KNOX_NCM
 	// KNOX NPA - START
 	struct timespec open_timespec;
 	// KNOX NPA - END
+#endif
 
 	/* We don't want any race condition at early drop stage */
 	atomic_inc(&net->ct.count);
@@ -1256,6 +1262,7 @@ __nf_conntrack_alloc(struct net *net,
 
 	spin_lock_init(&ct->lock);
 
+#ifdef CONFIG_KNOX_NCM
 	// KNOX NPA - START
 	/* initialize the conntrack structure members when memory is allocated */
 	if (ct != NULL) {
@@ -1278,6 +1285,7 @@ __nf_conntrack_alloc(struct net *net,
 		atomic_set(&ct->intermediateFlow, 0);
 	}
 	// KNOX NPA - END
+#endif
 
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple = *orig;
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].hnnode.pprev = NULL;
