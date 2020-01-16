582471abc67e0ab21f766a8e33248867bca1ba3e
Lars-Peter Clausen <lars@metafoo.de>
2020-01-16 15:29:01 +0200

    usb: gadget: ffs: ffs_aio_cancel(): Save/restore IRQ flags
    [ Upstream commit 43d565727a3a6fd24e37c7c2116475106af71806 ]
    
    ffs_aio_cancel() can be called from both interrupt and thread context. Make
    sure that the current IRQ state is saved and restored by using
    spin_{un,}lock_irq{save,restore}().
    
    Otherwise undefined behavior might occur.
    
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>
    Signed-off-by: Felipe Balbi <balbi@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    # Conflicts:
    #	drivers/usb/gadget/function/f_fs.c

--- drivers/usb/gadget/function/f_fs.c
+++ drivers/usb/gadget/function/f_fs.c
@@ -149,7 +149,7 @@ struct ffs_epfile {
 
 	/*
 	 * Buffer for holding data from partial reads which may happen since
-	 * we?™re rounding user read requests to a multiple of a max packet size.
+	 * we?ï¿½re rounding user read requests to a multiple of a max packet size.
 	 *
 	 * The pointer is initialised with NULL value and may be set by
 	 * __ffs_epfile_read_data function to point to a temporary buffer.
@@ -177,18 +177,18 @@ struct ffs_epfile {
 	 *   ?? __ffs_epfile_read_buffer_free: go to ptr == DROP
 	 *   ?? __ffs_epfile_read_buffered:    nop
 	 *   ?? __ffs_epfile_read_data allocates temp buffer: go to ptr == buf
-	 *   ?? reading finishes:              n/a, not in ?˜and reading?? state
+	 *   ?? reading finishes:              n/a, not in ?ï¿½and reading?? state
 	 * ?? ptr == DROP:
 	 *   ?? __ffs_epfile_read_buffer_free: nop
 	 *   ?? __ffs_epfile_read_buffered:    go to ptr == NULL
 	 *   ?? __ffs_epfile_read_data allocates temp buffer: free buf, nop
-	 *   ?? reading finishes:              n/a, not in ?˜and reading?? state
+	 *   ?? reading finishes:              n/a, not in ?ï¿½and reading?? state
 	 * ?? ptr == buf:
 	 *   ?? __ffs_epfile_read_buffer_free: free buf, go to ptr == DROP
 	 *   ?? __ffs_epfile_read_buffered:    go to ptr == NULL and reading
 	 *   ?? __ffs_epfile_read_data:        n/a, __ffs_epfile_read_buffered
 	 *                                    is always called first
-	 *   ?? reading finishes:              n/a, not in ?˜and reading?? state
+	 *   ?? reading finishes:              n/a, not in ?ï¿½and reading?? state
 	 * ?? ptr == NULL and reading:
 	 *   ?? __ffs_epfile_read_buffer_free: go to ptr == DROP and reading
 	 *   ?? __ffs_epfile_read_buffered:    n/a, mutex is held
@@ -782,7 +782,7 @@ static ssize_t ffs_copy_to_iter(void *data, int data_len, struct iov_iter *iter)
 	 * internally uses a larger, aligned buffer so that such UDCs are happy.
 	 *
 	 * Unfortunately, this means that host may send more data than was
-	 * requested in read(2) system call.  f_fs doesn?™t know what to do with
+	 * requested in read(2) system call.  f_fs doesn?ï¿½t know what to do with
 	 * that excess data so it simply drops it.
 	 *
 	 * Was the buffer aligned in the first place, no such problem would
@@ -790,7 +790,7 @@ static ssize_t ffs_copy_to_iter(void *data, int data_len, struct iov_iter *iter)
 	 *
 	 * Data may be dropped only in AIO reads.  Synchronous reads are handled
 	 * by splitting a request into multiple parts.  This splitting may still
-	 * be a problem though so it?™s likely best to align the buffer
+	 * be a problem though so it?ï¿½s likely best to align the buffer
 	 * regardless of it being AIO or not..
 	 *
 	 * This only affects OUT endpoints, i.e. reading data with a read(2),
@@ -991,7 +991,7 @@ static ssize_t ffs_epfile_io(struct file *file, struct ffs_io_data *io_data)
 		/*
 		 * Do we have buffered data from previous partial read?  Check
 		 * that for synchronous case only because we do not have
-		 * facility to ?˜wake up?? a pending asynchronous read and push
+		 * facility to ?ï¿½wake up?? a pending asynchronous read and push
 		 * buffered data to it which we would need to make things behave
 		 * consistently.
 		 */
@@ -1192,6 +1192,7 @@ static int ffs_aio_cancel(struct kiocb *kiocb)
 	struct ffs_io_data *io_data = kiocb->private;
 	struct ffs_epfile *epfile = kiocb->ki_filp->private_data;
 	struct ffs_data *ffs = epfile->ffs;
+	unsigned long flags;
 	int value;
 
 	ENTER();
@@ -1199,14 +1200,14 @@ static int ffs_aio_cancel(struct kiocb *kiocb)
 	ffs_log("enter:state %d setup_state %d flag %lu", ffs->state,
 		ffs->setup_state, ffs->flags);
 
-	spin_lock_irq(&epfile->ffs->eps_lock);
+	spin_lock_irqsave(&epfile->ffs->eps_lock, flags);
 
 	if (likely(io_data && io_data->ep && io_data->req))
 		value = usb_ep_dequeue(io_data->ep, io_data->req);
 	else
 		value = -EINVAL;
 
-	spin_unlock_irq(&epfile->ffs->eps_lock);
+	spin_unlock_irqrestore(&epfile->ffs->eps_lock, flags);
 
 	ffs_log("exit: value %d", value);
 
