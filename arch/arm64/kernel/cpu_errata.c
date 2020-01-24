b048c2d6b1c41b7fba6807977f465a17ad22f5a3
freeza <freeza@gmail.com>
2020-01-24 00:16:15 -0800

    Revert "arm64: Always enable ssb vulnerability detection"
    This reverts commit bef3560550f2dc217afe64047d4663e0c2f86b2b.

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -229,6 +229,7 @@ enable_smccc_arch_workaround_1(const struct arm64_cpu_capabilities *entry)
 }
 #endif	/* CONFIG_HARDEN_BRANCH_PREDICTOR */
 
+#ifdef CONFIG_ARM64_SSBD
 DEFINE_PER_CPU_READ_MOSTLY(u64, arm64_ssbd_callback_required);
 
 int ssbd_state __read_mostly = ARM64_SSBD_KERNEL;
@@ -265,11 +266,6 @@ early_param("ssbd", ssbd_cfg);
 
 void arm64_set_ssbd_mitigation(bool state)
 {
-	if (!IS_ENABLED(CONFIG_ARM64_SSBD)) {
-		pr_info_once("SSBD disabled by kernel configuration\n");
-		return;
-	}
-
 	if (this_cpu_has_cap(ARM64_SSBS)) {
 		if (state)
 			asm volatile(SET_PSTATE_SSBS(0));
@@ -389,6 +385,7 @@ out_printmsg:
 
 	return required;
 }
+#endif	/* CONFIG_ARM64_SSBD */
 
 #define CAP_MIDR_RANGE(model, v_min, r_min, v_max, r_max)	\
 	.matches = is_affected_midr_range,			\
@@ -632,12 +629,14 @@ const struct arm64_cpu_capabilities arm64_errata[] = {
 		.enable = enable_smccc_arch_workaround_1,
 	},
 #endif
+#ifdef CONFIG_ARM64_SSBD
 	{
 		.desc = "Speculative Store Bypass Disable",
 		.type = ARM64_CPUCAP_LOCAL_CPU_ERRATUM,
 		.capability = ARM64_SSBD,
 		.matches = has_ssbd_mitigation,
 	},
+#endif
 #ifdef CONFIG_ARM64_ERRATUM_1188873
 	{
 		.desc = "ARM erratum 1188873",
